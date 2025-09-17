eb9254d5f03483e989d6608c1cbf7be712dd40ec
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-17 18:37:44 +0530

    chore:supress warning

--- orders/models.py
+++ orders/models.py
@@ -31,7 +31,7 @@ class Order(models.Model):
         return f"Order {self.pk}"
 
     def get_total_cost(self):
-        total_cost = sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all()) # type: ignore
         return total_cost - total_cost * (self.discount / Decimal(100))
 
 
